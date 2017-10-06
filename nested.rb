76be9dcf160dfbbd478b3786ba6b0e42a73c83bf
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-06 03:31:50 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-		 programmer_hash[:grace_hopper]
+	programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,7 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-
+		 
 	programmer_hash[:alan_kay][:known_for]
 end
 
@@ -57,6 +57,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+	programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
